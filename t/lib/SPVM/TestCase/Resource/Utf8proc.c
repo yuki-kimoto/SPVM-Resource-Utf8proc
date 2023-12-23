f8100674f8eae93a052355a975fc70e6e18018ec
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-23 09:17:50 +0900

    Fix a warning.

--- t/lib/SPVM/TestCase/Resource/Utf8proc.c
+++ t/lib/SPVM/TestCase/Resource/Utf8proc.c
@@ -6,7 +6,7 @@ int32_t SPVM__TestCase__Resource__Utf8proc__test(SPVM_ENV* env, SPVM_VALUE* stac
   
   const char* string = "あいう";
   
-  char* string_ret_tmp = NULL;
+  utf8proc_uint8_t* string_ret_tmp = NULL;
   
   int32_t string_ret_length = utf8proc_map(string, 0, &string_ret_tmp, UTF8PROC_NULLTERM | UTF8PROC_STABLE |
   UTF8PROC_COMPOSE);
